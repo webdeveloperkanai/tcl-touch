15f70c01e420b1e18615f0a6d241e354ca253277
webdeveloperkanai <kanaishil501@gmail.com>
2023-09-05 14:12:40 +0530

    first commit

---
+++ main.tcl
@@ -0,0 +1,8 @@
+set myName "Kanai"
+set myTitle "Shil"
+set myAge 23.6
+# Printing my name - 
+puts "My Name is $myName $myTitle,"+" age is $myAge Y"
+
+puts "\n\nHello World"
+puts "This is test message"
