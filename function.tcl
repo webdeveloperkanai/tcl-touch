2db2b9ffa196e41dad9841c26119906a2981c4be
webdeveloperkanai <kanaishil501@gmail.com>
2023-09-05 19:32:25 +0530

    Functions, Concatinate

---
+++ function.tcl
@@ -0,0 +1,17 @@
+proc makeSum {a b} {
+    set result [expr {$a + $b} ]
+    return $result
+}
+
+proc makeDivide {a b} {
+    set result [expr {$a / $b}]
+    set rest [expr {$a % $b}]
+    return [concat $result "and rest" $rest] ; # concatinate :::)))))))))
+}
+set a 76
+set b 15
+
+set sum [makeSum $a $b];
+set devide [makeDivide $a $b];
+
+puts "The sum value is $sum and dividation value is $devide" 
