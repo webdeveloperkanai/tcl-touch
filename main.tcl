625b79f12be8e58d2649283dd5723c342fdecab2
webdeveloperkanai <kanaishil501@gmail.com>
2023-09-05 14:14:52 +0530

    Variables

--- main.tcl
+++ main.tcl
@@ -2,7 +2,7 @@ set myName "Kanai"
 set myTitle "Shil"
 set myAge 23.6
 # Printing my name - 
-puts "My Name is $myName $myTitle,"+" age is $myAge Y"
+puts "My Name is $myName $myTitle, age is $myAge Y"
 
 puts "\n\nHello World"
 puts "This is test message"
